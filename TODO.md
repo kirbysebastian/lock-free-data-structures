ab6d7ef56d9b5b4ed63d67030b45f621a0745342
kirbysebastian <kirbypaolo.sebastian@gmail.com>
2026-01-04 22:45:15 +0800

    implement atomicity

---
+++ TODO.md
@@ -0,0 +1,3 @@
+# Lists of TODOs
+1. Setup a linter
+2. Setup benchmark (prefer Google Benchmark)
--- data-structures/SPSCBoundedQueue.hpp
+++ data-structures/SPSCBoundedQueue.hpp
@@ -1,9 +1,16 @@
 #ifndef LOCK_FREE_DATA_STRUCTURE_SPSC_BOUNDED_QUEUE_HPP
 #define LOCK_FREE_DATA_STRUCTURE_SPSC_BOUNDED_QUEUE_HPP
 
-#include <array>
+#include <atomic>
+#include <memory>
 
 #include "utils.hpp"
+
+/*
+ * (& (N - 1)) is a mask similar with modulo but more efficient.
+ */
+
+#define CACHE_LINE_SIZE 64
 
 namespace lockfree {
 
@@ -13,33 +20,45 @@ class SPSCBoundedQueue final {
 public:
 
     bool tryPush(T&& value) noexcept {
-        if (isFull()) {
+		const auto currentHead{m_headIdx.load(std::memory_order::relaxed)};
+        if (N == (currentHead - m_tailIdx.load(std::memory_order::relaxed))) {
             return false;
         }
-        m_data[m_headIdx++ & (N - 1)] = std::forward<T>(value);
+
+        //m_data[currentHead & (N - 1)] = std::forward<T>(value); // NOTE: might throw, but should be fine, right??
+		std::construct_at(std::addressof(m_data[currentHead & (N - 1)]), value);
+		m_headIdx.store(currentHead + 1, std::memory_order::release);
         return true;
     }
 
     bool tryPop(T& value) noexcept {
-        if (isEmpty()) {
+		const auto currTail{m_tailIdx.load(std::memory_order::relaxed)};
+        if (currTail == m_headIdx.load(std::memory_order::acquire)) {
             return false;
         }
 
-        value = m_data[m_tailIdx++ & (N - 1)];
+        value = m_data[currTail & (N - 1)];
+		std::destroy_at(std::addressof(m_data[currTail & (N - 1)]));
+		m_tailIdx.store(currTail + 1, std::memory_order::relaxed);
         return true;
     }
 
+	/*
     template <class... Args>
     bool emplace(Args&&... args) noexcept {
         return false; // @TODO: Implement me!
     }
+	*/
 
     bool isFull() const noexcept {
-        return N == (m_headIdx - m_tailIdx);
+		return N == (
+		    m_headIdx.load(std::memory_order::relaxed) -
+				m_tailIdx.load(std::memory_order::relaxed));
     }
 
     bool isEmpty() const noexcept {
-        return m_headIdx == m_tailIdx;
+        return m_headIdx.load(std::memory_order::relaxed) ==
+			m_tailIdx.load(std::memory_order::relaxed);
     }
 
     constexpr std::size_t capacity() const noexcept {
@@ -47,18 +66,14 @@ public:
     }
 
     std::size_t size() const noexcept {
-        return m_headIdx - m_tailIdx;
-    }
-
-    // Troubleshoot
-    auto getHeadIdx() const {
-        return m_headIdx;
+        return m_headIdx.load(std::memory_order::relaxed) -
+			m_tailIdx.load(std::memory_order::relaxed);
     }
 
 private:
-    std::atomic<std::size_t> m_headIdx{};
-    std::atomic<std::size_t> m_tailIdx{};
-    std::array<T, N> m_data;
+    alignas(CACHE_LINE_SIZE) std::atomic<std::size_t> m_headIdx{};
+    alignas(CACHE_LINE_SIZE) std::atomic<std::size_t> m_tailIdx{};
+	T m_data[N];
 };
 
 } // namespace lockfree
